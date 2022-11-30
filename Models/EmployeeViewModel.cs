b322ada13ec06f81350451b6d70d1cbaddbdfaf5
irisy <ayao@my.wctc.edu>
2022-11-29 19:04:12 -0600

    add employee tab and manage discounts functions

--- Models/EmployeeViewModel.cs
+++ Models/EmployeeViewModel.cs
@@ -1,19 +1,19 @@
-// using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-// namespace Northwind.Models
-// {
-//     public class EmployeeWithPassword
-//     {
-//         public Employee Employee { get; set; }
-//         [UIHint("password"), Required]
-//         public string Password { get; set; }
-//     }
-//         public class SignInModel
-//     {
-//         [Required, UIHint("email")]
-//         public string Email { get; set; }
+namespace Northwind.Models
+{
+    public class EmployeeWithPassword
+    {
+        public Employee Employee { get; set; }
+        [UIHint("password"), Required]
+        public string Password { get; set; }
+    }
+        public class SignInModel
+    {
+        [Required, UIHint("email")]
+        public string Email { get; set; }
 
-//         [Required, UIHint("password")]
-//         public string Password { get; set; }
-//     }
-// }
+        [Required, UIHint("password")]
+        public string Password { get; set; }
+    }
+}
